b7e41fc23d48f823110c7dfc8e62c7a4845af6ab
gangzhaorige <gangzhaorige.li@gmail.com>
2022-09-13 11:45:29 -0700

    feat: added game server

---
+++ Server/src/metadata/Constants.java
@@ -0,0 +1,21 @@
+package metadata;
+
+/**
+ * The Constants class stores important variables as constants for later use.
+ */
+public class Constants {
+
+    // Request (1xx) + Response (2xx)
+    public final static short CMSG_AUTH = 101;
+    public final static short SMSG_AUTH = 201;
+    public final static short CMSG_HEARTBEAT = 102;
+    public final static short SMSG_HEARTBEAT = 202;
+    public final static short CMSG_PLAYERS = 103;
+    public final static short SMSG_PLAYERS = 203;
+    // Other
+    public static final float BIOMASS_SCALE = 1000;
+    public static final String CLIENT_VERSION = "1.00";
+    public static final int TIMEOUT_SECONDS = 90;
+    public static final String CSV_SAVE_PATH = "src/log/";
+
+}
---
+++ Server/src/networking/request/RequestPlayers.java
@@ -0,0 +1,26 @@
+package networking.request;
+
+import networking.response.ResponsePlayers;
+import utility.Log;
+
+import java.io.IOException;
+
+public class RequestPlayers extends GameRequest {
+
+    private ResponsePlayers responsePlayers;
+
+    //Do not remove this constructor even though the IDE claims it is not being used anywhere. It is being called by reflection.
+    public RequestPlayers() {
+        responsePlayers = new ResponsePlayers();
+        responses.add(responsePlayers);
+    }
+
+    @Override
+    public void parse() throws IOException {
+    }
+
+    @Override
+    public void doBusiness() throws Exception {
+        Log.printf("Number of players request received !!!");
+    }
+}
