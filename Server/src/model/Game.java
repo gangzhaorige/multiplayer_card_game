f97c816fd7254e76fa4f64c79ecbd46514d12728
gangzhaorige <gangzhaorige.li@gmail.com>
2022-09-20 20:16:52 -0700

    feat: added pile object to Game

--- Server/src/model/Game.java
+++ Server/src/model/Game.java
@@ -3,14 +3,17 @@ package model;
 import java.util.List;
 
 public class Game {
+
+    private Pile pile;
     private Deck deck;
     private List<Player> players;
 
     public Game(List<Player> players) {
         this.deck = new Deck();
+        this.pile = new Pile();
         this.players = players;
         for(Player p : players) {
-            deck.dealCard(p);
+            deck.dealCardToPlayer(p);
         }
     }
 
@@ -21,4 +24,9 @@ public class Game {
     public List<Player> getPlayers() {
         return this.players;
     }
+
+    public Pile getPile() {
+        return this.pile;
+    }
+
 }
